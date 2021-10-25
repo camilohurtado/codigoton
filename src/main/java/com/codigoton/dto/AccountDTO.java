18d87b4186004ccfc747bf5d53eee550aed901e8
Camilo Hurtado <camilohurtadocarvajal@hotmail.com>
2021-10-24 21:24:31 -0500

    Codigoton
    - Add config annotations to dtos

--- src/main/java/com/codigoton/dto/AccountDTO.java
+++ src/main/java/com/codigoton/dto/AccountDTO.java
@@ -1,7 +1,17 @@
 package com.codigoton.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
 /**
  * @author Camilo Hurtado
  */
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class AccountDTO {
+    private long id;
+    private long clientId;
+    private double balance;
 }
--- src/main/java/com/codigoton/dto/ClientDTO.java
+++ src/main/java/com/codigoton/dto/ClientDTO.java
@@ -1,7 +1,26 @@
 package com.codigoton.dto;
 
+import lombok.*;
+
+import java.util.List;
+
 /**
  * @author Camilo Hurtado
  */
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
 public class ClientDTO {
+
+    private long id;
+    private String code;
+    private char male;
+    private int type;
+    private String location;
+    private String company;
+    private char encrypt;
+
+    private List<AccountDTO> accounts;
 }
