1000099434e52b9fd6b6deccd396372094f259fa
Camilo Hurtado <camilohurtadocarvajal@hotmail.com>
2021-10-25 19:01:31 -0500

    Codigoton - config
    - Add annotation to config repositories
    - Add RestTemplate config
    - Add application properties file entries

--- src/main/java/com/codigoton/Codigoton.java
+++ src/main/java/com/codigoton/Codigoton.java
@@ -2,11 +2,13 @@ package com.codigoton;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * @author Camilo Hurtado
  */
 @SpringBootApplication
+@EnableJpaRepositories
 public class Codigoton {
 
     public static void main(String[] args) {
