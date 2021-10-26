0ceb6ad168b11ece86387c57932c3566b77cfcb4
Camilo Hurtado <camilohurtadocarvajal@hotmail.com>
2021-10-25 19:07:00 -0500

    Codigoton - Client information
    - Add two new methods on service interface
    - Add the respective implementation to the concrete class and fix naming from repository method

--- src/main/java/com/codigoton/service/information/ClientInfoServiceImpl.java
+++ src/main/java/com/codigoton/service/information/ClientInfoServiceImpl.java
@@ -1,7 +1,9 @@
 package com.codigoton.service.information;
 
+import com.codigoton.consumer.CodeDecryptConsumer;
 import com.codigoton.dto.AccountDTO;
 import com.codigoton.dto.ClientDTO;
+import com.codigoton.dto.FilterDTO;
 import com.codigoton.persistence.model.Account;
 import com.codigoton.persistence.model.Client;
 import com.codigoton.persistence.repository.AccountRepository;
@@ -9,6 +11,8 @@ import com.codigoton.persistence.repository.ClientRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -21,6 +25,7 @@ public class ClientInfoServiceImpl implements ClientInfoService{
 
     private final ClientRepository clientRepository;
     private final AccountRepository accountRepository;
+    private final CodeDecryptConsumer codeDecryptConsumer;
 
     @Override
     public ClientDTO getClientInformation(long clientId) throws Exception {
@@ -30,7 +35,7 @@ public class ClientInfoServiceImpl implements ClientInfoService{
                 .orElseThrow(() -> new Exception("The client does not exists."));
 
         List<AccountDTO> accountDTOList =
-                accountRepository.findAllByClientId(clientId)
+                accountRepository.findByClientId(clientId)
                 .stream()
                 .map(Account::toDTO)
                 .collect(Collectors.toList());
@@ -38,5 +43,39 @@ public class ClientInfoServiceImpl implements ClientInfoService{
         clientDTO.setAccounts(accountDTOList);
 
         return clientDTO;
+    }
+
+    @Override
+    public void setAccountInformation(ClientDTO clientDto) {
+        if(clientDto != null){
+            List<AccountDTO> accountDTOList =
+                    accountRepository.findByClientId(clientDto.getId())
+                            .stream()
+                            .map(Account::toDTO)
+                            .collect(Collectors.toList());
+
+            clientDto.setAccounts(accountDTOList);
+        }
+    }
+
+    @Override
+    public List<ClientDTO> getAllClientsByFilter(String location, int clientType) {
+        return clientRepository
+                .findByLocationAndType(location, clientType)
+                .stream()
+                .map(Client::toDTO)
+                .peek(clientDTO -> {
+                    if(clientDTO.getEncrypt() == '1'){
+                        String encryptedCode = clientDTO.getCode();
+                        try {
+                            clientDTO.setCode(codeDecryptConsumer.getDecryptedCode(encryptedCode));
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            clientDTO.setCode("FAILED");
+                        }
+                    }
+                })
+                .peek(this::setAccountInformation)
+                .collect(Collectors.toList());
     }
 }
