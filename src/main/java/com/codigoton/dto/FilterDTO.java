b872beecb1b87f86fdbd634edac6629c4ac5d8a8
Camilo Hurtado <camilohurtadocarvajal@hotmail.com>
2021-10-25 19:08:59 -0500

    Codigoton - FilterDto
    - Add properties to DTO class
    - Add data management annotations from lombok

--- src/main/java/com/codigoton/dto/FilterDTO.java
+++ src/main/java/com/codigoton/dto/FilterDTO.java
@@ -1,7 +1,21 @@
 package com.codigoton.dto;
 
+import lombok.*;
+
 /**
  * @author Camilo Hurtado
  */
+@Getter
+@Setter
+@Builder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
 public class FilterDTO {
+
+    private String mesa;
+    private int tipoDeCliente;
+    private String codUbicacionGeo;
+    private double rangoInicialBalance;
+    private double rangoFinalBalance;
+
 }
