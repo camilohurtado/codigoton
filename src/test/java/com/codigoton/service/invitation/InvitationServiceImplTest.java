40efbfec982a4c998f9459b79f1e2885f031da3c
Camilo Hurtado <camilohurtadocarvajal@hotmail.com>
2021-11-09 20:57:31 -0500

    Codigoton - unit test
    - Finish service unit test

--- src/test/java/com/codigoton/service/invitation/InvitationServiceImplTest.java
+++ src/test/java/com/codigoton/service/invitation/InvitationServiceImplTest.java
@@ -7,6 +7,7 @@ import com.codigoton.dto.InvitationResponse;
 import com.codigoton.persistence.model.Account;
 import com.codigoton.service.information.ClientInfoService;
 import org.assertj.core.api.Assertions;
+import org.assertj.core.api.ObjectEnumerableAssert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -14,10 +15,8 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -98,7 +97,7 @@ class InvitationServiceImplTest {
     }
 
     @Test
-    void setupInvitationsMoreThan4ClientsValidateResponse() {
+    void setupInvitationsMoreThan4ClientsNotEmpty() {
         Mockito.when(clientInfoServiceMock.getAllClientsByFilter(Mockito.anyString(), Mockito.anyInt())).thenReturn(getClients());
 
         FilterDTO filterDTO1 = FilterDTO.builder()
@@ -111,10 +110,63 @@ class InvitationServiceImplTest {
 
         List<InvitationResponse> result = sut.setupInvitations(List.of(filterDTO1));
 
-        //TODO - Finish unit test that validates business logic correctness
+        Assertions.assertThat(result).isNotEmpty();
+    }
+
+    @Test
+    void setupInvitationsMoreThan4ClientsValidateTablesInResponse() {
+        Mockito.when(clientInfoServiceMock.getAllClientsByFilter(Mockito.anyString(), Mockito.anyInt())).thenReturn(getClients());
+
+        FilterDTO filterDTO1 = FilterDTO.builder()
+                .tipoDeCliente(1)
+                .rangoInicialBalance(1000)
+                .rangoFinalBalance(2000)
+                .codUbicacionGeo("01")
+                .mesa("MESA01")
+                .build();
+
+        List<FilterDTO> filters = List.of(filterDTO1);
+
+        List<InvitationResponse> result = sut.setupInvitations(filters);
+
+        Assertions.assertThat(result)
+                        .extracting("table", String.class)
+                        .contains((filters.stream().map(filterDTO -> filterDTO.getMesa()).collect(Collectors.toList()).toArray(new String[0])));
+    }
+
+    @Test
+    void setupInvitationsMoreThan4ClientsValidateListOfGuests_expectedSize() {
+        Mockito.when(clientInfoServiceMock.getAllClientsByFilter(Mockito.anyString(), Mockito.anyInt())).thenReturn(getClients());
+
+        FilterDTO filterDTO1 = FilterDTO.builder()
+                .tipoDeCliente(1)
+                .rangoInicialBalance(1000)
+                .rangoFinalBalance(2000)
+                .codUbicacionGeo("01")
+                .mesa("MESA01")
+                .build();
+
+        List<FilterDTO> filters = List.of(filterDTO1);
+
+        List<InvitationResponse> result = sut.setupInvitations(filters);
+
+        List<List<String>> expectedListOfGuests = List.of(List.of("cod11", "cod01", "cod03", "cod06", "cod05", "cod07", "cod09"));
+
+        Assertions.assertThat(result)
+                    .map(InvitationResponse::getListOfGuests)
+                    .isEqualTo(expectedListOfGuests);
 
     }
 
+    /**
+     * Get less than 4 clients as follows (Client code = (account#:balance))
+     *
+     * cod01, male = (account1:300), (account2:1000)
+     * cod02, women = (account3:500)
+     * cod03, male = (account4:1900), (account5:100)
+     *
+     * @return List - {@link ClientDTO}
+     */
     private List<ClientDTO> getLessThan4Clients(){
 
         AccountDTO accountDTO1 = AccountDTO.builder()
@@ -164,7 +216,7 @@ class InvitationServiceImplTest {
                 .type(2)
                 .male('0')
                 .code("cod02")
-                .company("comp01")
+                .company("comp02")
                 .location("01")
                 .accounts(List.of(accountDTO3))
                 .build();
@@ -174,7 +226,7 @@ class InvitationServiceImplTest {
                 .type(1)
                 .male('1')
                 .code("cod03")
-                .company("comp01")
+                .company("comp03")
                 .location("02")
                 .accounts(List.of(accountDTO4, accountDTO5))
                 .build();
@@ -182,13 +234,70 @@ class InvitationServiceImplTest {
         return List.of(clientDTO1, clientDTO2, clientDTO3);
     }
 
+    /**
+     * Get more than 4 clients as follows (Client code = (account#:balance))
+     *
+     * cod01, male = (account1:300), (account2:1000)
+     * cod02, women = (account3:500)
+     * cod03, male = (account4:1900), (account5:100)
+     * cod05, women = (account6:2000)
+     * cod06, male = (account7:2000)
+     * cod07, women = (account8:2000)
+     * cod08, male = (account9:2000), (account10:2000)
+     * cod09, women = (account11:2000)
+     * cod10, women = (account12:100)
+     * cod11, women = (account13:1000)
+     *
+     * @return List - {@link ClientDTO}
+     */
     private List<ClientDTO> getClients(){
         AccountDTO accountDTO1 = AccountDTO.builder()
-                .clientId(1)
+                .clientId(5)
                 .balance(2000)
-                .id(1)
+                .id(6)
                 .build();
 
+        AccountDTO accountDTO2 = AccountDTO.builder()
+                .clientId(6)
+                .balance(2000)
+                .id(7)
+                .build();
+
+        AccountDTO accountDTO3 = AccountDTO.builder()
+                .clientId(7)
+                .balance(2000)
+                .id(8)
+                .build();
+
+        AccountDTO accountDTO4 = AccountDTO.builder()
+                .clientId(8)
+                .balance(2000)
+                .id(9)
+                .build();
+
+        AccountDTO accountDTO5 = AccountDTO.builder()
+                .clientId(8)
+                .balance(2000)
+                .id(10)
+                .build();
+
+        AccountDTO accountDTO6 = AccountDTO.builder()
+                .clientId(9)
+                .balance(2000)
+                .id(11)
+                .build();
+
+        AccountDTO accountDTO7 = AccountDTO.builder()
+                .clientId(10)
+                .balance(100)
+                .id(12)
+                .build();
+
+        AccountDTO accountDTO8 = AccountDTO.builder()
+                .clientId(11)
+                .balance(1000)
+                .id(13)
+                .build();
 
         ClientDTO clientDTO1 = ClientDTO.builder()
                 .id(5)
@@ -200,8 +309,75 @@ class InvitationServiceImplTest {
                 .accounts(List.of(accountDTO1))
                 .build();
 
+        ClientDTO clientDTO2 = ClientDTO.builder()
+                .id(6)
+                .type(1)
+                .male('1')
+                .code("cod06")
+                .company("comp05")
+                .location("01")
+                .accounts(List.of(accountDTO2))
+                .build();
+
+        ClientDTO clientDTO3 = ClientDTO.builder()
+                .id(7)
+                .type(1)
+                .male('0')
+                .code("cod07")
+                .company("comp06")
+                .location("01")
+                .accounts(List.of(accountDTO3))
+                .build();
+
+        ClientDTO clientDTO4 = ClientDTO.builder()
+                .id(8)
+                .type(1)
+                .male('1')
+                .code("cod08")
+                .company("comp07")
+                .location("01")
+                .accounts(List.of(accountDTO4, accountDTO5))
+                .build();
+
+        ClientDTO clientDTO5 = ClientDTO.builder()
+                .id(9)
+                .type(1)
+                .male('0')
+                .code("cod09")
+                .company("comp08")
+                .location("01")
+                .accounts(List.of(accountDTO6))
+                .build();
+
+        ClientDTO clientDTO6 = ClientDTO.builder()
+                .id(10)
+                .type(1)
+                .male('0')
+                .code("cod10")
+                .company("comp09")
+                .location("01")
+                .accounts(List.of(accountDTO7))
+                .build();
+
+        ClientDTO clientDTO7 = ClientDTO.builder()
+                .id(11)
+                .type(1)
+                .male('0')
+                .code("cod11")
+                .company("comp10")
+                .location("01")
+                .accounts(List.of(accountDTO8))
+                .build();
+
         List<ClientDTO> clientDTOS = new ArrayList<>();
         clientDTOS.add(clientDTO1);
+        clientDTOS.add(clientDTO2);
+        clientDTOS.add(clientDTO3);
+        clientDTOS.add(clientDTO4);
+        clientDTOS.add(clientDTO5);
+        clientDTOS.add(clientDTO6);
+        clientDTOS.add(clientDTO7);
+
         clientDTOS.addAll(getLessThan4Clients());
 
         return clientDTOS;
